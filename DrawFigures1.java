b257e0cbe258d00f3810ff55ce603fdbbe8ce89c
Shankatron <evan.shanks@techhigh.us>
2014-09-19 08:21:47 -0400

    Procedural decompostion for ESWT done or something

---
+++ DrawFigures1.java
@@ -0,0 +1,38 @@
+public class DrawFigures1 {
+       
+   public static void first() {
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println();
+        }
+   public static void second() {
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println();
+        }
+   public static void fourth() {
+         System.out.println("|United|");
+        System.out.println("|States|");
+     }
+   public static void third() {
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        }
+        
+    public static void main(String[] args) {
+     first();
+     second();
+     second();
+     first();
+     first();
+     third();
+     fourth();
+     third();
+     first();
+      
+    }
+}
