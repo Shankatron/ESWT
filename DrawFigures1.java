45e5e4c6490e27e27016cd5f534bddf3c979225c
Shankatron <evan.shanks@techhigh.us>
2014-09-19 09:33:42 -0400

    I Talked about how i felt/did with this project and what im working on next

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -23,7 +23,7 @@ public class DrawFigures1 {
         System.out.println("|      |");
         System.out.println("+------+");
         }
-        
+        /* at first i found this project to be really difficult because there was no internet. i was freaking out about it and that also threw me off track with the work. but with help/advice from friends in class i was able to understand the code and reconnect the internet to finish this project. glad to be done with it on to make a video*/
     public static void main(String[] args) {
      first();
      second();
